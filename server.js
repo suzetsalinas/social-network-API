4045a5d31c2b18e771d06aa0e3039a80ef99db41
Suzet Salinas <suzet.sal@gmail.com>
2024-02-01 19:22:07 -0600

    installed dotenv and express, created server.js file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true,useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'))
+
+const userRoutes = require('./routes/userRoutes');
+const thoughtRoutes = require('./routes/thoughtRoutes');
+
+app.use ('/user', userRoutes);
+app.use ('/thought', thoughtRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
